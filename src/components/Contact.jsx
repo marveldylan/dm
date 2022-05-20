6328c66eb1ed8d5d227691e39293e10553077e9d
Dylan Marvel <marvel.dylan.james@gmail.com>
2022-05-20 15:12:06 -0500

    git test

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,6 +6,7 @@ const Contact = () => {
     return (
         <div className="contact-container">
             <h1>Contact Linked</h1>
+            <h4>TESTESTTESTESTSETS</h4>
         </div>
     )
 }
