0a36f053ce9962450228e11c0db15117085b79f5
Dylan Marvel <marvel.dylan.james@gmail.com>
2022-05-20 15:33:40 -0500

    added for second test

--- src/components/MobileLanding.jsx
+++ src/components/MobileLanding.jsx
@@ -5,6 +5,7 @@ const MobileLanding = () => {
     return (
         <div className="Mobile-landing">
             <img className="Mobile-image" src="https://i.imgur.com/vJ3z4uT.png" />
+            <h4>Another test</h4>
         </div>
     )
 }
