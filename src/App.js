e8dfe393c993ed98b35e3a502ac116df5ae7a0e2
Dylan Marvel <marvel.dylan.james@gmail.com>
2022-05-20 13:55:56 -0500

    add website

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 import { ParallaxProvider } from 'react-scroll-parallax'
 import {Routes, Route } from 'react-router-dom'
 import About from './components/About'
 import Contact from './components/Contact'
 import Landing from './components/Landing'
-import Nav from './components/Nav'
+import MobileLanding from './components/MobileLanding';
 import Resume from './components/Resume'
 import Work from './components/Work'
 
 function App() {
+
+  const [windowWidth, setWidth] = useState(window.innerWidth)
+  const [windowHeight, setHeight] = useState(window.innerHeight)
+
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth)
+      setHeight(window.innerHeight)
+      console.log(window.innerWidth, 'width')
+      console.log(window.innerHeight, 'height')
+    }
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize)
+  }, [windowWidth, windowHeight])
+
   return (
     <div className="App">
       {/* <header className="App-header">
         <Nav />
       </header> */}
       <main className="App-main">
-        <Landing />
+        {
+          (windowWidth > 950) ?
+          <Landing />
+          : <MobileLanding />
+        }
+
         <About />
         <Work />
         <Contact />
